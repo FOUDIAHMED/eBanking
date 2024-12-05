00f5a9e250ad878be21cf85ccc6d29518c7a574d
ahmed <foudiahmed82@gmail.com>
2024-12-05 12:57:51 +0100

    security config

---
+++ src/main/java/ahmed/foudi/eBanking/security/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package ahmed.foudi.eBanking.security.config;
+
+import ahmed.foudi.eBanking.security.service.CustomAuthenticationProvider;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@AllArgsConstructor
+public class SecurityConfig {
+
+    private final CustomAuthenticationProvider authenticationProvider;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(AbstractHttpConfigurer::disable)
+            .sessionManagement(session -> 
+                session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .authorizeHttpRequests(auth -> {
+                auth.requestMatchers("/api/notice").permitAll()
+                     .requestMatchers("/api/contact").permitAll()
+                        .requestMatchers("/api/users/register").permitAll()
+                    .requestMatchers("/api/users/**").hasRole("ROLE_ADMIN")
+                        .requestMatchers("/api/users/{username}").hasRole("ROLE_USER")
+                    .anyRequest().authenticated();
+            })
+            .authenticationProvider(authenticationProvider);
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+} 
